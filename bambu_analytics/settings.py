9320fbad93ab8fbe809805d7b7c20d5f37e1feb5
Yaroslav Klyuyev <imposeren@gmail.com>
2014-11-08 11:25:52 +0200

    tabs to spaces, pep8

--- bambu_analytics/settings.py
+++ bambu_analytics/settings.py
@@ -11,13 +11,17 @@
 
 from django.conf import settings as s
 
-PROVIDER = getattr(s, 'ANALYTICS_PROVIDER',
+PROVIDER = getattr(
+    s, 'ANALYTICS_PROVIDER',
     'bambu_analytics.providers.google.UniversalAnalyticsProvider'
 )
 
+
 def get(klass):
-    return getattr(s,
-        'ANALYTICS_SETTINGS', {
+    return getattr(
+        s,
+        'ANALYTICS_SETTINGS',
+        {
             klass: {}
         }
     ).get(klass, {})
